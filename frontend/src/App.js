ba973ed018a58962ba9d75ee8db6d9136f67428b
Malas <csigerson2@huskers.unl.edu>
2025-02-02 17:51:42 -0600

    added worker uri

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-//import React, { useEffect, useState } from "react";
-//import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://express-worker.non300300.workers.dev/api/courses")
+            .then(response => setCourses(response.data))
+            .catch(error => console.error("API error:", error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Course List</h1>
+            <ul>
+                {courses.map(course => (
+                    <li key={course.id}>{course.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
