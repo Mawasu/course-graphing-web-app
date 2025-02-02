a93cc1bc61440a84757b6df4d5e98c1b27ff39e8
Malas <csigerson2@huskers.unl.edu>
2025-02-02 16:58:32 -0600

    fix errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+//import React, { useEffect, useState } from "react";
+//import axios from "axios";
 
 function App() {
   return (
